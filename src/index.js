02c049fd06f02c54d8b2c0716d7b38b8f76a733e
Thomas Kerbrat <kerbrat@outlook.com>
2016-08-19 00:24:33 +0200

    Basic initial implementation. - Constructor function. - Properties for storage type and availability. - get and set prototypes.

--- src/index.js
+++ src/index.js
@@ -18,13 +18,75 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-void function (webStorageLibrary) {
+void function (get_TKWebStorageLibrary) {
   if (window && !window.ws) {
-    window.ws = webStorageLibrary()
+    window.ws = get_TKWebStorageLibrary()
   }
   // TODO: AngularJS
-} (function WebStorageLibrary() {
-  return {
-    test: 'abc123'
+} (function () {
+
+  /**
+   * @param {string} type The name of the storage to test.
+   * @return {boolean} Returns if the browser supports and make available the tested storage.
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Feature-detecting_localStorage
+   */
+  function storageAvailable(type) {
+    try {
+      var storage = window[type]
+      var x = '__storage_test__'
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
+
+  /**
+   * @param {string} storage The type of storage you want to use. Possible values: "localStorage" (default) and "sessionStorage".
+   */
+  function TKWebStorageLibrary(storage) {
+
+    this.localStorageAvailable = storageAvailable('localStorage')
+    this.sessionStorageAvailable = storageAvailable('sessionStorage')
+
+    this.storageType = 'localStorage'
+    if (storage !== undefined) {
+      if (['localStorage', 'sessionStorage'].indexOf(storage) === -1) {
+        throw new Error('Constructor only accepts "localStorage" and "sessionStorage" for the storage parameter.')
+      } else {
+        this.storageType = storage
+      }
+    }
+
+    this.storage = window[this.storageType]
+
+  }
+
+  TKWebStorageLibrary.prototype.get = _getItem
+  TKWebStorageLibrary.prototype.getItem = _getItem
+
+  TKWebStorageLibrary.prototype.set = _setItem
+  TKWebStorageLibrary.prototype.setItem = _setItem
+
+  /**
+   * @name TKWebStorageLibrary.get
+   * @param {string} keyName The name of the key to get the value.
+   * @returns {null|string} The value stored under the keyName key, or null if nothing was found.
+   */
+  function _getItem(keyName) {
+    return this.storage.getItem(keyName)
+  }
+
+  /**
+   * @name TKWebStorageLibrary.set
+   * @param {string} keyName The name of the key to set the value.
+   * @param {string} keyValue The value to set.
+   * @returns {null|string} The value stored under the keyName key, or null if nothing was found.
+   */
+  function _setItem(keyName, keyValue) {
+    return this.storage.setItem(keyName, keyValue)
+  }
+
+  return TKWebStorageLibrary
 })
--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,137 @@
 var assert = chai.assert
 
-describe('Web Storage', function () {
+describe('window', function () {
+  it('should be defined', function () {
+    assert.isDefined(window)
+  })
+
   it('should expose localStorage', function () {
     assert.isDefined(localStorage)
   })
 
-  it('should expose the lib under window.ws', function () {
-    assert.isDefined(ws)
+  it('should expose sessionStorage', function () {
+    assert.isDefined(sessionStorage)
+  })
+})
+
+describe('The library', function () {
+  it('should expose itslef under window.ws.', function () {
+    assert.isDefined(window.ws)
+  })
+})
+
+describe('TKWebStorageLibrary', function () {
+
+  describe('constructor', function () {
+    it('should accept "localStorage" or "sessionStorage" in first parameter.', function () {
+      try {
+        assert.doesNotThrow(function () {
+          var sut1 = new ws('localStorage');
+          var sut2 = new ws('sessionStorage');
+        }, Error)
+      } catch (error) { }
+    })
+
+    it('should throw for any other value in first parameter.', function () {
+      assert.throw(function () {
+        var sut = new ws(0);
+      }, Error)
+    })
   })
 
-  describe('API', function () {
-    it('should expose test', function () {
-      assert.isDefined(ws.test)
-      assert.strictEqual(ws.test, 'abc123')
+  describe('property', function () {
+
+    var sut = new ws()
+
+    describe('storageType', function () {
+      it('should be defined.', function () {
+        assert.isDefined(sut.storageType)
+      })
+
+      it('should default to "localStorage".', function () {
+        assert.strictEqual(sut.storageType, 'localStorage')
+      })
+
+      it('should accept "sessionStorage".', function () {
+        var sut = new ws('sessionStorage')
+        assert.strictEqual(sut.storageType, 'sessionStorage')
+      })
     })
+
+    describe('localStorage', function () {
+      it('should be defined.', function () {
+        assert.isDefined(sut.localStorageAvailable)
+      })
+
+      it('should be true.', function () {
+        assert.strictEqual(sut.localStorageAvailable, true)
+      })
+    })
+
+    describe('sessionStorage', function () {
+      it('should be defined.', function () {
+        assert.isDefined(sut.sessionStorageAvailable)
+      })
+
+      it('should be true.', function () {
+        assert.strictEqual(sut.sessionStorageAvailable, true)
+      })
+    })
+  })
+
+  describe('prototype', function () {
+
+    describe('get', function () {
+
+      var sut
+
+      beforeEach('clear all entries', function () {
+        sut = new ws()
+        window.localStorage.clear()
+      })
+
+      it('should be defined.', function () {
+        assert.isDefined(sut.get)
+      })
+
+      it('should alias getItem.', function () {
+        assert.strictEqual(sut.get, sut.getItem)
+      })
+
+      it('should return null if a key does not exists.', function () {
+        assert.strictEqual(sut.get('a key that does not exists'), null)
+      })
+    })
+
+    describe('set', function () {
+
+      var sut
+
+      beforeEach('clear all entries', function () {
+        sut = new ws()
+        window.localStorage.clear()
+      })
+
+      it('should be defined.', function () {
+        assert.isDefined(sut.set)
+      })
+
+      it('should alias setItem.', function () {
+        assert.strictEqual(sut.set, sut.setItem)
+      })
+
+      it('should set an item in the localStorage.', function () {
+        sut.set('key', 'value')
+        assert.strictEqual(window.localStorage.getItem('key'), 'value')
+      })
+
+      it('should set a value even if the key is already present', function () {
+        window.localStorage.setItem('key', 'value')
+        assert.strictEqual(window.localStorage.getItem('key'), 'value')
+        sut.set('key', 'value2')
+        assert.strictEqual(window.localStorage.getItem('key'), 'value2')
+      })
+    })
+
   })
 })
